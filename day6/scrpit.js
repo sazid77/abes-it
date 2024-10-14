b399117dc50471b1f7286bc65018fcaf40234306
sazid77 <sazidkhan79821@gmail.com>
2024-10-14 13:05:51 +0530

    day6

---
+++ day6/scrpit.js
@@ -0,0 +1,53 @@
+// let a=()=>{
+//     console.log("arrow function");
+// }
+
+// let b=()=>console.log("arrow funtion 2");
+// b();
+// a();
+
+
+// let c=(x,y)=>{
+//     return x+y;
+// }
+// let res =c("ravi","rahul");
+// let res1=c(12,23);
+// console.log(res,res1)
+
+// function display(){
+//     let p1=document.querySelector('p');
+//     p1.innerHTML="this is my first para";
+//     p1.style.backgroundColor="red";
+// }
+
+
+
+// function display(){
+//     let p1=document.querySelectorAll('p');
+//     p1[0].innerHTML="this is my first para";
+//     p1[0].style.backgroundColor="red";
+//     p1[1].innerHTML="this is my second  para";
+//     p1[1].style.backgroundColor="green";
+// }
+
+
+// let changeheader = ()=>{
+//        let head1=document.querySelector("h1")
+//        head1.textContent="change the title";
+//        let a1=document.querySelector("a")
+//        a1.setAttribute("href","https://aktu.ac.in/")
+//        a1.innerHTML="aktu website";
+// }
+
+
+
+// let addcontent =()=>{
+//     const newDiv = document.createElement('div');
+//     newDiv.textContent="this is new div";
+//     const body= document.querySelector('body');
+//     body.appendChild(newDiv);
+// }
+
+
+
+
